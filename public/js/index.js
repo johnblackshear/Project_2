ce9288a58ed0b32fb72b998c5c6c5d72c2f943c7
Allison Champlin <47014575+AllisonMChamplin@users.noreply.github.com>
2019-08-03 01:06:22 -0500

    John Home search

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,7 @@
 
 // Get the search string User types in
-var $clubNameSearchTerm = $('#club-id-search');
-var $clubIdSearchTerm = $('#club-name-search');
+var $clubNameSearchTerm = $('#club-name-search');
+var $clubIdSearchTerm = $('#club-id-search');
 // Search submit buttons
 var $nameSubmitBtn = $('#club-name-search-submit');
 var $idSubmitBtn = $('#club-id-search-submit');
@@ -38,15 +38,35 @@ var API = {
 var handleClubNameSearch = function (event) {
     event.preventDefault();
     // console.log("clicky");
-    var search = $clubIdSearchTerm.val().trim();
+    var search = $clubNameSearchTerm.val().trim();
     console.log("search term: ", search);
     if (!(search)) {
         alert('You must enter a search term!')
         return
     };
-    API.nameSearchClubs(search).then(function (results) {
-        console.log("results: ", results);
-        $clubNameResults.append(JSON.stringify(results));
+    API.nameSearchClubs(search).then(function (data) {
+        console.log("data: ", data);
+        console.log("data.length: ", data.length);
+
+        for (var i = 0; i < data.length; i++) {
+            var $clubid = data[i].id;
+            var $clubname = data[i].clubname;
+
+            var $a = $('<a>')
+                .text($clubname)
+                .attr('href', '/clubs/' + $clubid);
+
+            var $li = $('<li>')
+                .append('Club Name: ')
+                .append($a)
+                .append('<br>Club Id: ' + $clubid);
+
+            $clubNameResults.empty();
+            $clubNameResults.append('<h3>Results:</h3>');
+            $clubNameResults.append($li);
+        }
+
+
     });
 };
 
@@ -54,19 +74,38 @@ var handleClubNameSearch = function (event) {
 var handleClubIdSearch = function (event) {
     event.preventDefault();
     // console.log("clicky");    
-    var search = $clubNameSearchTerm.val().trim();
+    var search = $clubIdSearchTerm.val().trim();
     console.log("search term: ", search);
     if (!(search)) {
         alert('You must enter a search term!')
         return
     };
-    API.idSearchClubs(search).then(function (results) {
-        console.log("results: ", results);
-        $clubIdResults.append(JSON.stringify(results));
+    API.idSearchClubs(search).then(function (data) {
+        console.log("data: ", data);
+
+        var $clubid = data.id;
+        var $clubname = data.clubname;
+
+        var $a = $('<a>')
+            .text($clubname)
+            .attr('href', '/clubs/' + $clubid);
+
+        var $li = $('<li>')
+            .append('Club Name: ')
+            .append($a)
+            .append('<br>Club Id: ' + $clubid);
+
+        // return $li;
+        $clubIdResults.empty();
+        $clubIdResults.append('<h3>Results:</h3>');
+        $clubIdResults.append($li);
     });
+
 };
 
 // Add event listener to the submit button
 $nameSubmitBtn.on('click', handleClubNameSearch);
 $idSubmitBtn.on('click', handleClubIdSearch);
+
+
 
